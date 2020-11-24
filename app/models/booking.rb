1567f3b375b490be9a7035cf7b63e15f33d7b451
Marvin Pflaume <pflaume.marvin@gmail.com>
2020-11-24 12:23:50 +0100

    Added relevant data to dashboard

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   enum status: { pending: 0, confirmed: 1, declined: 2 }
 
   belongs_to :user
-  belongs_to :object
+  belongs_to :item
 
   validates :start_date, presence: true
   validates :end_date, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items
-  resources :bookings, only: [:show, :new, :create, :edit, :update]
+  resources :items do
+    resources :bookings, only: [:create]
+  end
+  resources :bookings, only: [:show, :edit, :update]
 end
