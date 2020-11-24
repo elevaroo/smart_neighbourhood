8e60b18987fd2cec6a20b55ff60b51e46ec7815d
Marvin Pflaume <pflaume.marvin@gmail.com>
2020-11-24 17:28:57 +0100

    fixed routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :skip => [:registrations]
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    patch 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end
+  devise_for :users
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   get '/bookings/:id/accept',  to: 'bookings#accept', as: 'accept'
