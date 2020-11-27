381b9755f44e123254a05ca8de0a16cf29ba0d38
Marvin Pflaume <pflaume.marvin@gmail.com>
2020-11-27 13:43:08 +0100

    fixed design dashboard, updated tabs, fixed general style issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  before_save :capitalize_names
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def capitalize_names
+    self.first_name = first_name.camelcase
+    self.last_name = last_name.camelcase
+  end
+
   has_many :items
   has_many :bookings
   has_one_attached :photo
